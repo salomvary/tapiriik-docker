9c54e1174f17f8676218e5d3a244f8c5ea1e9620
Márton Salomváry <salomvary@gmail.com>
2015-12-12 00:11:33 +0100

    Keep sync_worker running infinitely

--- sync_worker.sh
+++ sync_worker.sh
@@ -4,5 +4,9 @@ set -euf
 # Wait for RabbitMQ to boot up
 while ! nc -q 1 rabbitmq 5672 </dev/null; do sleep 1; done
 
-# Chokes on non-absolute path!
-python3 /tapiriik/sync_worker.py
+# The worker shuts down after completing n tasks
+# https://github.com/cpfair/tapiriik/issues/191#issuecomment-164065625
+while true; do
+	# Chokes on non-absolute path!
+	python3 /tapiriik/sync_worker.py
+done
